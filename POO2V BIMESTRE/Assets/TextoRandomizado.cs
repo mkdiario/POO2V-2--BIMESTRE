29c53e716da1e39ec7e3171e8c6db35f7ff85a97
mkdiario <myrella.santos@escolar.ifrn.edu.br>
2025-07-07 13:53:28 -0300

    nao deu certo

--- POO2V BIMESTRE/Assets/TextoRandomizado.cs
+++ POO2V BIMESTRE/Assets/TextoRandomizado.cs
@@ -1,16 +1,37 @@
+
 using TMPro;
 using UnityEngine;
 
-public class TextoRabdomizado : MonoBehaviour
+
+
+public class TextoRandomizado : MonoBehaviour
 {
     public string novoTexto;
     public TextMeshProUGUI UITexto;
-    
+
+
+    public int numero;
+
+    private void Start()
+    {
+        UITexto.text = novoTexto + " " + numero;
+    }
+
     void Update()
     {
-     if (Input.GetKeyDown(KeyCode.Space))
-     { 
-         UITexto.text = novoTexto;
-     }
+        if (Input.GetKeyDown(KeyCode.Space) && numero > 0)
+        {
+
+            numero--;
+            UITexto.text = novoTexto + " " + numero;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+
+            numero++;
+            UITexto.text = novoTexto + " " + numero;
+        }
+
     }
-}   
+}
